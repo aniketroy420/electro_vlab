68d691fd2bd2ffc0ace277a8c3f59a85c07a28da
Aniket Roy <56649588+aniketroy420@users.noreply.github.com>
2023-07-03 12:32:41 +0530

    Update constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+int resistorValue = 0;
+List<bool> accepted = [
+ //Individual Booleans for each and every DragTarget
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+];
+int x0 = 0; // Individual variables for each and every dragTarget
+int x1 = 0;
+int x2 = 0;
+int x3 = 0;
+int x4 = 0;
+int x5 = 0;
+int x6 = 0;
+int x7 = 0;
+111
+int x8 = 0;
+int x9 = 0;
+int x10 = 0;
+int x11 = 0;
+int x12 = 0;
+int x13 = 0;
+int x14 = 0;
+int x15 = 0;
+//Width color and height for horizontal wires
+List<double> hWidth = [
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0
+];
+List<double> hHeight = [
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0
+
+];
+List<Color> hColor = [
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+];
+//width height and color for vertical wires
+List<double> vWidth = [
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0,
+ 27.0
+];
+List<double> vHeight = [
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0,
+ 50.0
+];
+List<Color> vColor = [
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+ Colors.transparent,
+];
+List<bool> animationHorizontal = [
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+];
+
+List<bool> animationVertical = [
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+ false,
+];
