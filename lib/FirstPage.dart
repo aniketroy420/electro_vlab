cf158caed453b2fa51e6bb816660a69adcc8cb7f
Aniket Roy <56649588+aniketroy420@users.noreply.github.com>
2023-07-03 12:38:28 +0530

    Update FirstPage.dart

--- lib/FirstPage.dart
+++ lib/FirstPage.dart
@@ -1,14 +1,66 @@
-
 import 'package:flutter/material.dart';
+import 'FirstPractical.dart';
+import 'SecondPractical.dart';
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePage();
-}
+import 'experiment.dart';
+class FirstPage extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+ return Scaffold(
+ appBar: AppBar(
+ title: Text('Electronic V-Lab'),
+ ),
+ body: Column(
+ children: [
+ ListTile(
+ leading: Icon(Icons.account_tree_outlined),
+ title: Text('Ohm\'s Law'),
+ onTap: () {
+ Navigator.push(
+ context,
+ MaterialPageRoute(
+ builder: (context) => FirstPractical(),
+ ),
+ );
+ },
+ ),
+ ListTile(
+ leading: Icon(Icons.account_tree_outlined),
+ title: Text('Kirchhoff\'s Law (Under Development)'),
+ onTap: () {
+ Navigator.push(
+ context,
+ MaterialPageRoute(
+ builder: (context) => SecondPractical(),
+ ),
+ );
+ },
+ ),
+ ListTile(
+ leading: Icon(Icons.account_tree_outlined),
 
-class _MyHomePage extends State<MyHomePage>{
-
-
-
-
+ title: Text('Experiment 3'),
+ onTap: () {
+ Navigator.push(
+ context,
+ MaterialPageRoute(
+ builder: (context) => Experiments(),
+ ),
+ );
+ },
+ ),
+ ListTile(
+ leading: Icon(Icons.account_tree_outlined),
+ title: Text('Experiment 4'),
+ onTap: () {},
+ ),
+ ListTile(
+ leading: Icon(Icons.account_tree_outlined),
+ title: Text('Experiment 5'),
+ onTap: () {},
+ ),
+ ],
+ ),
+ );
+ }
 }
